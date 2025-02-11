186d8ac48118f326eab61a9ee160ccb21a484879
Bryant Orme <99993554+Bryant-Orme@users.noreply.github.com>
2025-02-11 13:38:16 -0800

    Working version of NewHireADAutomation.ps1

--- New-User-Creation/NewHireADAutomation.ps1
+++ New-User-Creation/NewHireADAutomation.ps1
@@ -1,33 +1,37 @@
-﻿$PASSWORD_FOR_NEW_USERS = "changeme"
+$PASSWORD_FOR_NEW_USERS = "Changeme1"
 $NEW_HIRE_INFO          = Import-Csv -Path .\newHireInfo.csv
 
 $password = ConvertTo-SecureString $PASSWORD_FOR_NEW_USERS -AsPlainText -Force
 
 foreach ($row in $NEW_HIRE_INFO){
-    $firstName = $row.fName
-    $lastName = $row.lName
-    $displayName = "$($firstName) $($lastName)"
-    $initials = "$($firstName.SubString(0,1))$($lastName.SubString(0,1))"
-    $department = $row.Department
-    $email = "$($displayName)@xyz.com"
-    $phoneNumber = $row.pNum -replace "~[0-9]", ''
-    $city = $row.city
-    $state = $row.state
-    $country = $row.country
+    $firstName    = $row.fName
+    $lastName     = $row.lName
+    $displayName  = "$($firstName) $($lastName)"
+    $initials     = "$($firstName.SubString(0,1))$($lastName.SubString(0,1))"
+    $department   = $row.department
+    $email        = "$($firstName.SubString(0,1).ToLower())$($lastName.ToLower())@xyz.com"
+    $phoneNumber  = $row.pNum -replace "[^0-9]+", ''
+    $city         = $row.city
+    $state        = $row.state
+    $country      = $row.country
+    $group        = $row.group
 
     Write-Host "Creating user: $($displayName)" -BackgroundColor Black -ForegroundColor White
     
-    New-ADUser -AccountPassword $password `
-               -ChangePasswordAtLogOn $true `
-               -Name $displayName `
+    New-ADUser -Name $displayName `
+               -AccountPassword $password `
+               -Path "ou=_USERS,DC=XYZ,DC=com" `
+               -ChangePasswordAtLogon $true `
+               -GivenName $firstName `
+               -Surname $lastName `
                -Initials $initials `
+               -EmployeeID $displayName `
                -Department $department `
                -EmailAddress $email `
                -HomePhone $phoneNumber `
                -City $city `
                -State $state `
                -Country $country `
-               -Path "ou=_USERS,$(([ADSI]`"").distinguishedName)"`
-               -Enabled $true
-     
+               -AccountExpirationDate (Get-Date).AddDays(70) `
+               -Enabled $true `
 }
