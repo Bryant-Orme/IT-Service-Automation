587a05251d79f752a27c02682a90d318707f29b0
Bryant Orme <99993554+Bryant-Orme@users.noreply.github.com>
2025-02-10 18:34:16 -0800

    Untested, first upload of code structure

---
+++ NewHireADAutomation.ps1
@@ -0,0 +1,33 @@
+﻿$PASSWORD_FOR_NEW_USERS = "changeme"
+$NEW_HIRE_INFO          = Import-Csv -Path .\newHireInfo.csv
+
+$password = ConvertTo-SecureString $PASSWORD_FOR_NEW_USERS -AsPlainText -Force
+
+foreach ($row in $NEW_HIRE_INFO){
+    $firstName = $row.fName
+    $lastName = $row.lName
+    $displayName = "$($firstName) $($lastName)"
+    $initials = "$($firstName.SubString(0,1))$($lastName.SubString(0,1))"
+    $department = $row.Department
+    $email = "$($displayName)@xyz.com"
+    $phoneNumber = $row.pNum -replace "~[0-9]", ''
+    $city = $row.city
+    $state = $row.state
+    $country = $row.country
+
+    Write-Host "Creating user: $($displayName)" -BackgroundColor Black -ForegroundColor White
+    
+    New-ADUser -AccountPassword $password `
+               -ChangePasswordAtLogOn $true `
+               -Name $displayName `
+               -Initials $initials `
+               -Department $department `
+               -EmailAddress $email `
+               -HomePhone $phoneNumber `
+               -City $city `
+               -State $state `
+               -Country $country `
+               -Path "ou=_USERS,$(([ADSI]`"").distinguishedName)"`
+               -Enabled $true
+     
+}
