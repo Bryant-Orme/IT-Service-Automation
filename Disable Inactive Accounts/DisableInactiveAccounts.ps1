c3b201d19fe5d2342630f0788d75c58a4606a6d3
Bryant Orme <99993554+Bryant-Orme@users.noreply.github.com>
2025-02-11 18:02:59 -0800

    Single DC Disable Inactive Accounts Script

---
+++ Disable Inactive Accounts/DisableInactiveAccounts.ps1
@@ -0,0 +1,13 @@
+﻿### This script is meant to be run on each DC, as Search-ADAccount only 
+### runs on the local DC server
+
+$userList = Get-ADUser -Filter * -Properties AccountExpirationDate, LastLogonDate, WhenCreated | ? {
+                       ($_.AccountExpirationDate -NE $NULL -AND $_.AccountExpirationDate -LT (Get-Date)) -OR `
+                       ($_.LastLogonDate -NE $NULL -AND $_.LastLogonDate -LT (Get-Date).AddDays(-45))
+}
+            
+
+foreach ($u in $userList){
+   Disable-ADAccount $u
+   Write-Host Disabling $($u)
+}
