1bcd952b0db383eb423e778d70926e0fc5f478cb
Bryant Orme <99993554+Bryant-Orme@users.noreply.github.com>
2025-02-13 17:46:53 -0500

    Update DisableInactiveAccounts.ps1
    Changed script functionality to not disable accounts that are in the Administrator group.

--- Disable Inactive Accounts/DisableInactiveAccounts.ps1
+++ Disable Inactive Accounts/DisableInactiveAccounts.ps1
@@ -1,13 +1,13 @@
-﻿### This script is meant to be run on each DC, as Search-ADAccount only 
-### runs on the local DC server
+#Disable accounts that have been inactive for 45+ days
+#Script does not disable Administrator accounts that have been inactive
 
-$userList = Get-ADUser -Filter * -Properties AccountExpirationDate, LastLogonDate, WhenCreated | ? {
-                       ($_.AccountExpirationDate -NE $NULL -AND $_.AccountExpirationDate -LT (Get-Date)) -OR `
-                       ($_.LastLogonDate -NE $NULL -AND $_.LastLogonDate -LT (Get-Date).AddDays(-45))
-}
-            
+$adminList = Get-ADGroupMember -Identity "Administrators" | Select-Object -Property "name"
+$userList = Get-ADUser -Filter *  -Properties LastLogonDate | ? {($_.LastLogonDate -NE $NULL -AND $_.LastLogonDate -LT (Get-Date).AddDays(-45) -AND $_.Enabled -EQ $true)}
+
 
 foreach ($u in $userList){
-   Disable-ADAccount $u
-   Write-Host Disabling $($u)
+    if (([array]::IndexOf($adminList.name, $u.Name) -LT 0)) {
+        Disable-ADAccount $u
+        Write-Host Disabling $($u.SamAccountName) 
+    }
 }
