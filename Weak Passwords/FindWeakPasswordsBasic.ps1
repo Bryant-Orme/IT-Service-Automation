63cf8f996f7088b5f6481e1f71e5f36f28e74189
Bryant Orme <99993554+Bryant-Orme@users.noreply.github.com>
2025-02-12 21:32:12 -0800

    Add files via upload

---
+++ Weak Passwords/FindWeakPasswordsBasic.ps1
@@ -0,0 +1,12 @@
+﻿$reportPath = ".\WeakPasswordReport.csv"
+
+#Get accounts with potentially weak passwords
+$stalePasswords   = Get-ADUser -Filter * -Properties pwdLastSet | ? {($_.pwdLastSet -NE 0 -AND [DateTime]::FromFileTime($_.pwdLastSet) -LT (Get-Date).AddDays(-365))} 
+$noPasswordsUsers = Get-ADUser -Filter {PasswordNotRequired -EQ $true}
+$neverExpireUsers = Get-ADUser -Filter {PasswordNeverExpires -EQ $true -AND Endabled -EQ $true}
+
+#Export Results
+$stalePasswords | Export-Csv -Path $reportPath -Append -NoTypeInformation
+$noPasswordsUsers | Select-Object -Skip 1 | Export-Csv -Path $reportPath -Append -NoTypeInformation  
+$neverExpireUsers | Select-Object -Skip 1 | Export-Csv -Path $reportPath -Append -NoTypeInformation -Force
+
